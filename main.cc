7890c86e6c81282563b1bbaaa815d2a9affe89af
Clavier <y2595zhang@edu.wwaterloo.ca>
2018-11-21 03:51:26 -0500

    233

--- main.cc
+++ main.cc
@@ -18,30 +18,24 @@ int main () {
             cin.ignore();
             continue;
         }
-        // create a grid of size n
         if (command == "left") {
-            game.move("left", 1);
+            int step;
+            cin >> step;
+            game.move("left", step);
         } else if (command == "right") {
             int step;
             cin >> step;
             game.move("right", step);
         } else if (command == "down") {
-            game.move("down", 1);
-        } else if (command == "rotate") {
-            game.rotate(true, 1);
+            int step;
+            cin >> step;
+            game.move("down", step);
         } else if (command == "drop") {
             game.drop();
         } else if (command == "level") {
             int level;
             cin >> level;
             game.setLevel(level);
-        }
-        // n steps
-        else if (command == "steps") {
-
-        // print
-        } else if (command == "print") {
-            
         }
     } 
 }
